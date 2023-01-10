bff340ee93a7672c631027e0047b453f086290c0
daveman-creator <djustin43@gmail.com>
2023-01-10 22:05:41 +0100

    Modified the code

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('hello');
+import chalk from 'chalk';
+import randomcolor from 'randomcolor';
+
+const lum = process.argv[3];
+const col = process.argv[2];
+
+const color = randomcolor({
+  luminosity: lum,
+  hue: col,
+});
+
+function colorAndLog(str) {
+  console.log(chalk.hex(color).bold(str));
+}
+
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("########              ########");
+// colorAndLog(`########   ${color}    ########`);
+// colorAndLog("########              ########");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+
+colorAndLog(`##############################
+##############################
+##############################
+########              ########
+########   ${color}    ########
+########              ########
+##############################
+##############################
+##############################`);
